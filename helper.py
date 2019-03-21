e5b244ce62e2cb021138bf984e297fc734192323
vi <dev.facereal@gmail.com>
2019-03-21 14:58:59 +0700

    first commit

---
+++ helper.py
@@ -0,0 +1,232 @@
+
+import tensorflow as tf
+import numpy as np
+import re
+
+
+def model_inputs(embed_dim, reinforcement=False):
+    word_vectors = tf.placeholder(
+        tf.float32, [None, None, embed_dim], name="word_vectors")
+    reward = tf.placeholder(tf.float32, shape=(), name="rewards")
+    caption = tf.placeholder(tf.int32, [None, None], name="caption")
+    caption_mask = tf.placeholder(
+        tf.float32, [None, None], name="caption_mask")
+    if reinforcement:
+        # with reinforcement learning, there is an extra placeholder for reward
+        return word_vectors, caption, caption_mask, reward
+    else:
+        # normal training returns only the word_vectors, captions and caption_masks placeholders
+        return word_vectors, caption, caption_mask
+
+
+def encoding_layer(word_vectors, lstm_size, num_layers, keep_prob, vocab_size):
+    cells = tf.contrib.rnn.MultiRNNCell([
+        tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.LSTMCell(
+            lstm_size), keep_prob) for _ in range(num_layers)
+    ])
+
+    outputs, state = tf.nn.dynamic_rnn(cells, word_vectors, dtype=tf.float32)
+
+    return outputs, state
+
+
+def decode_train(enc_state, dec_cell, dec_input,
+                 target_sequence_length, output_sequence_length,
+                 outputs_layer, keep_prop):
+    dec_cell = tf.contrib.rnn.DropoutWrapper(
+        dec_cell, output_keep_prob=keep_prop)  # apply dropout to the LSTM cell
+    helper = tf.contrib.seq2seq.TrainingHelper(
+        dec_input, target_sequence_length)  # training helper for decoder
+    decoder = tf.contrib.seq2seq.BasicDecoder(
+        dec_cell, helper, enc_state, outputs_layer)
+    # unrolling the decoder layer
+    outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(
+        decoder, impute_finished=True, maximum_iterations=output_sequence_length)
+
+    return outputs
+
+
+def decode_generate(encoder_state, dec_cell, dec_embeddings,
+                    target_sequence_length, output_sequence_length,
+                    vocab_size, output_layer, batch_size, keep_prob):
+
+    dec_cell = tf.contrib.rnn.DropoutWrapper(
+        dec_cell, output_keep_prob=keep_prob)
+
+    # decoder helper for inference
+    helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(
+        dec_embeddings, tf.fill([batch_size], 1), 2)
+
+    decoder = tf.contrib.seq2seq.BasicDecoder(
+        dec_cell, helper, encoder_state, output_layer)
+
+    outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(
+        decoder,
+        impute_finished=True,
+        maximum_iterations=output_sequence_length)
+
+    return outputs
+
+def decoding_layer(dec_input, enc_state, 
+                   target_sequence_length, output_sequence_length,
+                   lstm_size, num_layers, n_words,
+                   batch_size, keep_prob, embeding_size, Train = True):
+    target_vocab_size = n_words
+    with tf.device("/cpu:0"):
+        dec_embeddings = tf.Variable(
+                tf.random_uniform([target_vocab_size, embeding_size], -0.1, 0.1),
+                name = 'Wemb')
+    dec_embed_input = tf.nn.embedding_lookup(dec_embeddings, dec_input)
+    cells = tf.contrib.rnn.MultiRNNCell(
+            [tf.contrib.rnn.LSTMCell(lstm_size) for _ in range(num_layers)])
+    with tf.variable_scope("decode"):
+        output_layer = tf.layers.Dense(target_vocab_size)
+    if Train:
+        with tf.variable_scope("decode"):
+            train_output = decode_train(enc_state, cells, dec_embed_input,
+                                        target_sequence_length,
+                                        output_sequence_length,
+                                        output_layer,
+                                        keep_prob)
+    with tf.variable_scope("decode", reuse=tf.AUTO_REUSE):
+        infer_output = decode_generate(enc_state, cells, dec_embeddings,
+                                       target_sequence_length,
+                                       output_sequence_length,
+                                       target_vocab_size,
+                                       output_layer,
+                                       batch_size,
+                                       keep_prob)
+    if Train:
+        return train_output, infer_output
+    return infer_output
+
+def bos_inclusion(caption,batch_size):
+    sliced_target = tf.strided_slice(caption, [0,0], [batch_size, -1], [1,1])
+    concat = tf.concat([tf.fill([batch_size, 1],1), sliced_target], 1)
+    return concat
+
+def pad_sequences(questions, sequence_length = 22):
+    lengths = [len(x) for x in questions]
+    num_samples = len(questions)
+    x = np.zeros((num_samples, sequence_length)).astype(int)
+    for idx, sequence in enumerate(questions):
+        if not len(sequence):
+            continue
+        truncated = sequence[-sequence_length:]
+        truncated = np.asarray(truncated, dtype=int)
+        x[idx, :len(truncated)] = truncated
+    return x
+
+def refine(data):
+    words = re.findall("[a-zA-Z'-]+", data)
+    words = ["".join(word.split("'")) for word in words]
+    data = ' '.join(words)
+    return data
+
+def make_batch_input(batch_input, input_sequence_length, embed_dims, word2vec):
+    for i in range(len(batch_input)):
+        batch_input[i] = [word2vec[w] if w in word2vec else np.zeros(embed_dims) for w in batch_input[i]]
+        if len(batch_input[i]) > input_sequence_length:
+            batch_input[i] = batch_input[i][ :input_sequence_length]
+        else:
+            for _ in range(input_sequence_length - len(batch_input[i])):
+                batch_input[i].append(np.zeros(embed_dims))
+    return np.array(batch_input)
+
+def replace(target, symbols): # remove special symbols
+    for symbol in symbols:
+        target =  list(map(lambda x: x.replace(symbol,''), target))
+        
+    return target
+
+def make_batch_target(batch_target, word_to_index, target_sequence_length):
+    target = batch_target
+    target = list(map(lambda x: '<bos> ' + x, target))
+    symbols = ['.', ',', '"', '\n', '?', '!', '\\', '/']
+    target = replace(target,symbols)
+    
+    for idx, each_cap in enumerate(target):
+        word = each_cap.lower().split(' ')
+        if len(word) < target_sequence_length:
+            target[idx] = target[idx] + ' <eos>' # append the end symbol
+        else:
+            new_word = ''
+            for i in range(target_sequence_length-1):
+                new_word = new_word + word[i] + ' '
+            target[idx] = new_word + '<eos>'
+    target_index = [
+            [word_to_index[word] if word in word_to_index else word_to_index['<unk>'] 
+            for word in sequence.lower().split(' ')] for sequence in target]
+    # print(target_index[0])
+    caption_matrix = pad_sequences(target_index, target_sequence_length)
+    caption_matrix = np.hstack([caption_matrix, np.zeros([len(caption_matrix), 1])]).astype(int)
+    
+    caption_masks = np.zeros((caption_matrix.shape[0], caption_matrix.shape[1]))
+    nonzeros = np.array(list(map(lambda x: (x != 0).sum(), caption_matrix)))
+    # print(nonzeros)
+    # print(caption_matrix[1])
+    for ind, row in enumerate(caption_masks): # set the masks as an array of ones where actual words exist and zeros otherwise
+        row[:nonzeros[ind]] = 1
+        # print(row)
+        print(caption_masks[0])
+        print(caption_matrix[0])
+        
+    return caption_matrix, caption_masks
+    
+def generic_batch(generic_response,  batch_size, 
+                  word_to_index, target_sequence_length):
+    size = len(generic_response)
+    if size > batch_size:
+        generic_response = generic_response[:batch_size]
+    else:
+        for j in range(batch_size-size):
+            # kv note
+            generic_response.append('')
+    return make_batch_target(generic_response, word_to_index, target_sequence_length)
+
+def index2sentence(generated_word_index, prob_logit, ixtoword): 
+    """ if the predicted word is 'unknown,<unk>--index == 3, replace with the second most probable word
+    Also if the predicted word is <pad> representing a pad or <bos> replace with the next most probable word
+    """
+    for i in range(len(generated_word_index)):
+        if generated_word_index[i] == 3 or generated_word_index[i] <= 1:
+            sort_prob_logit = sorted(prob_logit[i])
+            curindex = np.where(prob_logit[i] == sort_prob_logit[-2])[0][0]
+            count = 1
+            while curindex <= 3:
+                curindex = np.where(prob_logit[i] == sort_prob_logit[(-2)-count])[0][0]
+                count += 1
+
+            generated_word_index[i] = curindex
+
+    generated_words = []
+    for ind in generated_word_index:
+        generated_words.append(ixtoword[ind])
+
+    # generate sentence
+    punctuation = np.argmax(np.array(generated_words) == '<eos>') + 1    #The sentence ends where the punctuation <eos> is found The rest of the sentence is truncated
+    generated_words = generated_words[:punctuation]
+    generated_sentence = ' '.join(generated_words)
+
+    """ Modify the output sentence to take off '<eos>, <bos>, '--' 
+    Start every sentence with a capital letter, replace i, i'm i'd with I, I'm. I'd respectively and end with a full stop '.'
+    """
+    generated_sentence = generated_sentence.replace('<bos> ', '')
+    generated_sentence = generated_sentence.replace('<eos>', '')
+    generated_sentence = generated_sentence.replace('--', '')
+    generated_sentence = generated_sentence.split('  ')
+    for i in range(len(generated_sentence)):
+        generated_sentence[i] = generated_sentence[i].strip() 
+        if len(generated_sentence[i]) > 1:
+            generated_sentence[i] = generated_sentence[i][0].upper() + generated_sentence[i][1:] + '.'
+        else:
+            generated_sentence[i] = generated_sentence[i].upper()
+    generated_sentence = ' '.join(generated_sentence)
+    generated_sentence = generated_sentence.replace(' i ', ' I ')
+    generated_sentence = generated_sentence.replace("i'm", "I'm")
+    generated_sentence = generated_sentence.replace("i'd", "I'd")
+
+    return generated_sentence
+
+
+
---
+++ train.py
@@ -0,0 +1,259 @@
+# -*- coding: utf-8 -*-
+
+from data_reader import Data_Reader
+import data_parser
+from gensim.models import KeyedVectors
+import helper as h
+from seq_model import Chatbot
+import tensorflow as tf
+import numpy as np
+
+generic_responses = [
+        "Toi khong biet",
+        "Khong biet",
+        "Khong khong",
+        "khong biet",
+        "toi khong biet",
+        "khong khong"
+        ]
+
+checkpoint = True
+forward_model_path = "model/forward"
+reversed_model_path = "model/reversed"
+rl_model_path = "model/rl"
+model_name = "seq2seq"
+word_count_threshold = 1
+reversed_word_count_threshold = 1
+dim_word_vec = 300
+# dim_hidden = 1000
+dim_hidden = 512
+input_sequence_length = 22
+output_sequence_length = 22
+learning_rate = 0.0001
+epochs = 1
+batch_size = 200
+forward_ = "forward"
+reverse_ = "reverse"
+forward_epochs = 50
+reverse_epochs = 50
+display_interval = 100
+
+
+def train(type_, epochs=epochs, checkpoint=False):
+    tf.reset_default_graph()
+#     dr = Data_Reader()
+    if type_ == "forward":
+        path = "model/forward/seq2seq"
+        dr = Data_Reader(reverse=False)
+    else:
+        dr = Data_Reader(reverse=True)
+        path = "model/reverse/seq2seq"
+
+    word_to_index, index_to_word, _ = data_parser.preProBuildWordVocab(
+            word_count_threshold=word_count_threshold)
+
+    word_vector = KeyedVectors.load_word2vec_format(
+            'model/word_vector.bin', binary=True)
+
+    model = Chatbot(dim_word_vec, len(word_to_index), dim_hidden,
+                    batch_size,
+                    input_sequence_length,
+                    output_sequence_length,
+                    learning_rate)
+
+    optimizer, place_holders, predictions, logits, losses = model.build_model()
+
+    saver = tf.train.Saver()
+    sess = tf.InteractiveSession()
+
+    if checkpoint:
+        saver.restore(sess, path)
+        print("checkpoint restored at path: {}".format(path))
+    else:
+        tf.global_variables_initializer().run()
+
+    for epoch in range(epochs):
+        n_batch = dr.get_batch_num(batch_size=batch_size)
+        for batch in range(n_batch):
+            batch_input, batch_target = dr.generate_training_batch(batch_size)
+
+            inputs_ = h.make_batch_input(batch_input,
+                                         input_sequence_length,
+                                         dim_word_vec,
+                                         word_vector)
+            targets, masks = h.make_batch_target(batch_target,
+                                                 word_to_index,
+                                                 output_sequence_length)
+            feed_dict = {
+                    place_holders['word_vectors']: inputs_,
+                    place_holders['caption']: targets,
+                    place_holders['caption_mask']: masks
+                    }
+
+            _, loss_val, preds = sess.run(
+                    [optimizer, losses["entropy"], predictions],
+                    feed_dict=feed_dict)
+
+            if batch % display_interval == 0:
+                print(preds.shape)
+                print("Epoch: {}, batch: {}, loss: {}".format(
+                    epoch, batch, loss_val))
+
+        saver.save(sess, path)
+
+        print("Model saved at {}".format(path))
+
+    print("Training done")
+
+    sess.close()
+
+
+def pg_train(epochs=epochs, checkpoint=False):
+    tf.reset_default_graph()
+    path = "model/reinforcement/seq2seq"
+    word_to_indedx, index_to_word, _ = data_parser.preProBuildWordVocab(
+            word_count_threshold=word_count_threshold)
+    word_vectors = KeyedVectors.load_word2vec_format(
+        'model/word_vector.bin', binary=True)
+    generic_caption, generic_mask = h.generic_batch(
+            generic_responses,
+            batch_size, word_to_index,
+            output_sequence_length)
+
+#     dr = Data_Reader()
+
+    forward_graph = tf.Graph()
+    reverse_graph = tf.Graph()
+    default_graph = tf.get_default_graph()
+
+    with forward_graph.as_default():
+        pg_model = Chatbot(dim_word_vec, len(word_to_indedx),
+                           dim_hidden, batch_size,
+                           input_sequence_length,
+                           output_sequence_length,
+                           learning_rate,
+                           policy_gradients=True)
+        optimizer, place_holders, predictions, logits, losses = pg_model.build_model()
+
+        sess = tf.InteractiveSession()
+        saver = tf.train.Saver()
+        if checkpoint:
+            saver.restore(sess, path)
+            print("checkpoint restored at path: {}".format(path))
+        else:
+            tf.global_variables_initializer().run()
+            saver.restore(sess, 'model/forward/seq2seq')
+
+    with reverse_graph.as_default():
+        model = Chatbot(dim_word_vec, len(word_to_indedx), dim_hidden,
+                        batch_size, input_sequence_length,
+                        output_sequence_length, learning_rate)
+        _, rev_place_holder, _, _, reverse_loss = model.build_model()
+        sess2 = tf.InteractiveSession()
+        saver2 = tf.train.Saver()
+        saver2.restore(sess2, "model/reversed/seq2seq")
+        print("reverse model restored")
+
+    dr = Data_Reader(load_list=True)
+
+    for epoch in range(epochs):
+        n_batch = dr.get_batch_num(batch_size=batch_size)
+        for batch in range(n_batch):
+            # kv note
+            batch_input, batch_caption, prev_utterance = dr.generate_training_batch_with_former(
+                batch_size)
+            targets, masks = h.make_batch_target(
+                    batch_caption,
+                    word_to_indedx,
+                    output_sequence_length)
+
+            inputs_ = h.make_batch_input(batch_input,
+                                         input_sequence_length,
+                                         dim_word_vec,
+                                         word_vectors)
+            word_indices, probabilities = sess.run(
+                    [predictions, logits],
+                    feed_dict={
+                            place_holders['word_vectors']: inputs_,
+                            place_holders['caption']: targets
+                            })
+            sentence = [h.index2sentence(
+                    generated_word,
+                    probability,
+                    index_to_word)
+                    for generated_word, probability in
+                    zip(word_indices, probabilities)]
+
+            word_list = [word.split() for word in sentence]
+
+            generic_test_input = h.make_batch_input(
+                    word_list,
+                    input_sequence_length,
+                    dim_word_vec,
+                    word_vectors)
+
+            forward_coherence_target, forward_coherence_masks = h.make_batch_target(
+                    sentence, word_to_indedx, output_sequence_length)
+
+            generic_loss = 0.0
+
+            for response in generic_test_input:
+                sentence_input = np.array([response]*batch_size)
+
+                feed_dict = {place_holders['word_vectors']: sentence_input,
+                             place_holders['caption']: generic_caption,
+                             place_holders['caption_mask']: generic_mask
+                             }
+
+                generic_loss_i = sess.run(
+                    losses['entropy'], feed_dict=feed_dict)
+                generic_loss -= generic_loss_i/batch_size
+            # print ("generic loss work: {}". format (generic_loss) )
+            feed_dict = {
+                    place_holders['word_vectors']: inputs_,
+                    place_holders['caption']: forward_coherence_target,
+                    place_holders['caption_mask']: forward_coherence_masks
+                    }
+            forward_entropy = sess. run(losses["entropy"], feed_dict=feed_dict)
+            previous_utterance, previous_mask = h.make_batch_target(
+                    prev_utterance,
+                    word_to_indedx,
+                    output_sequence_length)
+
+            feed_dict = {
+                        rev_place_holders['word_vectors']: generic_test_input,
+                        rev_place_holders['caption']: previous_utterance,
+                        rev_place_holders['caption_mask']: previous_mask,
+                        }
+
+            reverse_entropy = sess2.run(reverse_loss["entropy"],
+                                         feed_dict=feed_dict)
+
+            rewards = 1 / (1 + np.exp(
+                    -reverse_entropy - forward_entropy - generic_loss))
+
+            feed_dict = {
+                        place_holders['word_vectors']: inputs_,
+                        place_holders['caption']: targets,
+                        place_holders['caption_mask']: masks,
+                        place_holders['rewards']: rewards
+                        }
+            _, loss_pg, loss_ent = sess.run(
+                    [optimizer, losses["pg"], losses["entropy"]],
+                    feed_dict=feed_dict)
+
+            if batch % display_interval == 0:
+                print("Epoch: {}, batch: {}, Entropy loss: {}, Policy gradient loss: {}".format(
+                        epoch, batch, loss_ent, loss_pg))
+                print("rewards: {}".format(rewards))
+
+        saver.save(sess, path)
+        print("Model saved add {}".format(path))
+    print("Training done")
+
+
+if __name__ == "__main__":
+    	
+    train(forward_, forward_epochs, False)
+    train(reverse_, reverse_epochs, False)
+    pg_train(100, False)
